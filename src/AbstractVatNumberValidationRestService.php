4da9dc1134f38d1cce629d4ffd2b6c0edc2c4216
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-23 19:23:44 +0400

    [ECVS-5] injected client.

--- src/AbstractVatNumberValidationRestService.php
+++ src/AbstractVatNumberValidationRestService.php
@@ -2,12 +2,21 @@
 
 namespace rocketfellows\ViesVatValidationRest;
 
+use GuzzleHttp\Client;
 use rocketfellows\ViesVatValidationInterface\VatNumber;
 use rocketfellows\ViesVatValidationInterface\VatNumberValidationResult;
 use rocketfellows\ViesVatValidationInterface\VatNumberValidationServiceInterface;
 
 abstract class AbstractVatNumberValidationRestService implements VatNumberValidationServiceInterface
 {
+    private $client;
+
+    public function __construct(
+        Client $client
+    ) {
+        $this->client = $client;
+    }
+
     public function validateVat(VatNumber $vatNumber): VatNumberValidationResult
     {
         // TODO: Implement validateVat() method.
