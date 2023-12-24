593c9de075f6dcf509b1a5cb68b8d8c1f8c655b4
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-24 19:59:34 +0400

    [ECVS-5] setup test.

--- tests/integration/VatNumberValidationRestServiceTest.php
+++ tests/integration/VatNumberValidationRestServiceTest.php
@@ -2,11 +2,25 @@
 
 namespace rocketfellows\ViesVatValidationRest\tests\integration;
 
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
+use rocketfellows\ViesVatValidationInterface\FaultCodeExceptionFactory;
+use rocketfellows\ViesVatValidationRest\services\VatNumberValidationRestService;
 
 /**
  * @group vies-vat-validation-rest
  */
 class VatNumberValidationRestServiceTest extends TestCase
 {
+    private $vatNumberValidationRestService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->vatNumberValidationRestService = new VatNumberValidationRestService(
+            (new Client()),
+            (new FaultCodeExceptionFactory())
+        );
+    }
 }
