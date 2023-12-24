a7688f80f89ff8a492778ee33e61e1eff931b577
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-24 20:59:57 +0400

    [ECVS-5] created test.

---
+++ tests/unit/helpers/ResponseErrorFactoryTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rocketfellows\ViesVatValidationRest\tests\unit\helpers;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group vies-vat-validation-rest-helpers
+ */
+class ResponseErrorFactoryTest extends TestCase
+{
+}
