495ce36855abf5af6deaf3b2ae850fc042285cd2
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-24 21:03:00 +0400

    [ECVS-5] added get response error message test stub.

--- tests/unit/helpers/ResponseErrorFactoryTest.php
+++ tests/unit/helpers/ResponseErrorFactoryTest.php
@@ -3,10 +3,32 @@
 namespace rocketfellows\ViesVatValidationRest\tests\unit\helpers;
 
 use PHPUnit\Framework\TestCase;
+use rocketfellows\ViesVatValidationRest\helpers\ResponseErrorFactory;
+use stdClass;
 
 /**
  * @group vies-vat-validation-rest-helpers
  */
 class ResponseErrorFactoryTest extends TestCase
 {
+    /**
+     * @dataProvider getResponseErrorMessageProvidedData
+     */
+    public function testGetResponseErrorMessage(stdClass $responseData, ?string $expectedResponseErrorMessage): void
+    {
+        $this->assertEquals(
+            $expectedResponseErrorMessage,
+            ResponseErrorFactory::getResponseErrorMessage($responseData)
+        );
+    }
+
+    public function getResponseErrorMessageProvidedData(): array
+    {
+        return [
+            [
+                'responseData',
+                'expectedResponseErrorMessage',
+            ],
+        ];
+    }
 }
