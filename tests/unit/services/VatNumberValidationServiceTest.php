b25f2b5d21003906e279de677765a32312804161
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-24 22:22:40 +0400

    [ECVS-5] added test case: response country code set, vat number set, request date set, is valid, name set, address set

--- tests/unit/services/VatNumberValidationServiceTest.php
+++ tests/unit/services/VatNumberValidationServiceTest.php
@@ -4,10 +4,14 @@ namespace rocketfellows\ViesVatValidationRest\tests\unit\services;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 use rocketfellows\ViesVatValidationInterface\exceptions\ServiceRequestException;
 use rocketfellows\ViesVatValidationInterface\FaultCodeExceptionFactory;
 use rocketfellows\ViesVatValidationInterface\VatNumber;
+use rocketfellows\ViesVatValidationInterface\VatNumberValidationResult;
 use rocketfellows\ViesVatValidationInterface\VatNumberValidationServiceInterface;
 
 /**
@@ -63,6 +67,150 @@ abstract class VatNumberValidationServiceTest extends TestCase
         $this->vatNumberValidationRestService->validateVat(
             $this->getValidatingVatNumberTestValue()
         );
+    }
+
+    /**
+     * @dataProvider getCheckVatProvidedData
+     */
+    public function testSuccessCheckVat(
+        VatNumber $vatNumber,
+        array $checkVatCallArgs,
+        string $checkVatResponse,
+        VatNumberValidationResult $expectedVatNumberValidationResult
+    ): void {
+        $this->client
+            ->expects($this->once())
+            ->method('post')
+            ->with(...$checkVatCallArgs)
+            ->willReturn($this->getResponseMock(['body' => $checkVatResponse,]));
+
+        $this->assertEquals(
+            $expectedVatNumberValidationResult,
+            $this->vatNumberValidationRestService->validateVat($vatNumber)
+        );
+    }
+
+    public function getCheckVatProvidedData(): array
+    {
+        return [
+            'response country code set, vat number set, request date set, is valid, name set, address set' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'checkVatResponse' => '{"countryCode" : "DE", "vatNumber" : "12312312", "requestDate" : "2023-11-11 23:23:23", "valid" : true, "name" : "foo",  "address" : "bar"}',
+                'expectedVatNumberValidationResult' => new VatNumberValidationResult(
+                    new VatNumber('DE', '12312312'),
+                    '2023-11-11 23:23:23',
+                    true,
+                    'foo',
+                    'bar'
+                ),
+            ],
+            /*'response country code set, vat number set, request date set, not valid, name set, address set' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'checkVatResponse' => (object) [
+                    'countryCode' => 'DE',
+                    'vatNumber' => '12312312',
+                    'requestDate' => '2023-11-11 23:23:23',
+                    'valid' => false,
+                    'name' => 'foo',
+                    'address' => 'bar',
+                ],
+                'expectedVatNumberValidationResult' => new VatNumberValidationResult(
+                    new VatNumber('DE', '12312312'),
+                    '2023-11-11 23:23:23',
+                    false,
+                    'foo',
+                    'bar'
+                ),
+            ],
+            'response country code not set, vat number not set, request date not set, validation not set, name not set, address not set' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'checkVatResponse' => (object) [],
+                'expectedVatNumberValidationResult' => new VatNumberValidationResult(
+                    new VatNumber('', ''),
+                    '',
+                    false,
+                    null,
+                    null
+                ),
+            ],
+            'response country code empty, vat number empty, request date empty, not valid, name empty, address empty' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'checkVatResponse' => (object) [
+                    'countryCode' => '',
+                    'vatNumber' => '',
+                    'requestDate' => '',
+                    'valid' => false,
+                    'name' => '',
+                    'address' => '',
+                ],
+                'expectedVatNumberValidationResult' => new VatNumberValidationResult(
+                    new VatNumber('', ''),
+                    '',
+                    false,
+                    '',
+                    ''
+                ),
+            ],*/
+        ];
+    }
+
+    private function getResponseMock(array $params = []): MockObject
+    {
+        $stream = $this->createMock(StreamInterface::class);
+        $stream->method('__toString')->willReturn($params['body'] ?? '');
+
+        $mock = $this->createMock(ResponseInterface::class);
+        $mock->method('getBody')->willReturn($stream);
+
+        return $mock;
     }
 
     private function getValidatingVatNumberTestValue(): VatNumber
