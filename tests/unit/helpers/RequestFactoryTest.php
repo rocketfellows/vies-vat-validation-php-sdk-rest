b375cee6c698b2008775f64a18b222dbc563ec87
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-24 20:43:44 +0400

    [ECVS-5] added test case: country code set, vat number set.

--- tests/unit/helpers/RequestFactoryTest.php
+++ tests/unit/helpers/RequestFactoryTest.php
@@ -3,6 +3,7 @@
 namespace rocketfellows\ViesVatValidationRest\tests\unit\helpers;
 
 use PHPUnit\Framework\TestCase;
+use rocketfellows\ViesVatValidationInterface\VatNumber;
 
 /**
  * @group vies-vat-validation-rest-helpers
@@ -12,9 +13,12 @@ class RequestFactoryTest extends TestCase
     public function getCheckVatNumberRequestProvidedData(): array
     {
         return [
-            [
-                'vatNumber',
-                'expectedRequestData',
+            'country code set, vat number set' => [
+                'vatNumber' => new VatNumber('DE', '1213'),
+                'expectedRequestData' => [
+                    'countryCode' => 'DE',
+                    'vatNumber' => '1213',
+                ],
             ],
         ];
     }
