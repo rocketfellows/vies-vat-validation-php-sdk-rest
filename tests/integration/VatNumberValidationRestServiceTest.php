36219da06e7fc239b909d64dee1a5dd685aad03c
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-24 19:58:08 +0400

    [ECVS-5] created vat number validation rest service test.

---
+++ tests/integration/VatNumberValidationRestServiceTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rocketfellows\ViesVatValidationRest\tests\integration;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group vies-vat-validation-rest
+ */
+class VatNumberValidationRestServiceTest extends TestCase
+{
+}
