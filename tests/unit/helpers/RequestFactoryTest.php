f980b97547577017c807c91b0774f4afbc3f8c1d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-24 20:41:27 +0400

    [ECVS-5] created test class.

---
+++ tests/unit/helpers/RequestFactoryTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rocketfellows\ViesVatValidationRest\tests\unit\helpers;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group vies-vat-validation-rest-helpers
+ */
+class RequestFactoryTest extends TestCase
+{
+}
