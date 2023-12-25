db189f43fc7dfb9c938ca64122832f6ae717a415
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-25 15:25:11 +0400

    [ECVS-5] implemented get response data.

--- src/helpers/ResponseFactory.php
+++ src/helpers/ResponseFactory.php
@@ -2,12 +2,20 @@
 
 namespace rocketfellows\ViesVatValidationRest\helpers;
 
+use Psr\Http\Message\ResponseInterface;
 use rocketfellows\ViesVatValidationInterface\VatNumber;
 use rocketfellows\ViesVatValidationInterface\VatNumberValidationResult;
 use stdClass;
 
 class ResponseFactory
 {
+    public static function getResponseData(ResponseInterface $response): stdClass
+    {
+        $responseData = json_decode((string) $response->getBody());
+
+        return ($responseData instanceof stdClass) ? $responseData : (object) [];
+    }
+
     public static function getVatNumberValidationResult(stdClass $responseData): VatNumberValidationResult
     {
         return VatNumberValidationResult::create(
