d13cd64d805ec4beb5f10bacab2305bab83d2d72
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-25 15:30:30 +0400

    [ECVS-5] implemented response data emptiness checker.

--- src/helpers/ResponseFactory.php
+++ src/helpers/ResponseFactory.php
@@ -9,6 +9,11 @@ use stdClass;
 
 class ResponseFactory
 {
+    public static function isResponseDataEmpty(stdClass $responseData): bool
+    {
+        return empty((array) $responseData);
+    }
+
     public static function getResponseData(ResponseInterface $response): stdClass
     {
         $responseData = json_decode((string) $response->getBody());
