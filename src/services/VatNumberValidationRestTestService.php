a531e901d41ee64a0a755f7673076b9090347dc7
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-26 10:51:59 +0400

    [ECVS-5] implemented.

--- src/services/VatNumberValidationRestTestService.php
+++ src/services/VatNumberValidationRestTestService.php
@@ -2,6 +2,12 @@
 
 namespace rocketfellows\ViesVatValidationRest\services;
 
-class VatNumberValidationRestTestService
+use rocketfellows\ViesVatValidationRest\AbstractVatNumberValidationRestService;
+
+class VatNumberValidationRestTestService extends AbstractVatNumberValidationRestService
 {
+    protected function getUrl(): string
+    {
+        return 'https://ec.europa.eu/taxation_customs/vies/rest-api/check-vat-test-service';
+    }
 }
