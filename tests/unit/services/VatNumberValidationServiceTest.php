fb7800f5a548d7cca0e9b03750b9a480280ba0ee
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-26 14:59:07 +0400

    [ECVS-5] added test case: thrown client exception, error code set, error code not set, error message not set.

--- tests/unit/services/VatNumberValidationServiceTest.php
+++ tests/unit/services/VatNumberValidationServiceTest.php
@@ -791,6 +791,30 @@ abstract class VatNumberValidationServiceTest extends TestCase
                     null
                 ),
             ],
+            'thrown client exception, error code set, error code not set, error message not set' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'thrownRequestException' => $this->getClientExceptionMock([
+                    'response' => $this->getResponseMock(['body' => '{"errorWrappers": [{}]}']),
+                ]),
+                'expectedException' => new UnknownServiceErrorException(
+                    '',
+                    '',
+                    0,
+                    null
+                ),
+            ],
             'thrown client exception, error code set, error code INVALID_INPUT, error message set' => [
                 'vatNumber' => new VatNumber(
                     'DE',
