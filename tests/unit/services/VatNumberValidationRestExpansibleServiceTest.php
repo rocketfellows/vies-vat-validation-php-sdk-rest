e4008e37e57a2dd546fb8f206e0461cde834f2a4
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-26 14:49:38 +0400

    [ECVS-5] covered VatNumberValidationRestExpansibleService.

---
+++ tests/unit/services/VatNumberValidationRestExpansibleServiceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace rocketfellows\ViesVatValidationRest\tests\unit\services;
+
+use rocketfellows\ViesVatValidationInterface\VatNumberValidationServiceInterface;
+use rocketfellows\ViesVatValidationRest\services\VatNumberValidationRestExpansibleService;
+
+/**
+ * @group vies-vat-validation-rest
+ */
+class VatNumberValidationRestExpansibleServiceTest extends VatNumberValidationServiceTest
+{
+    protected const EXPECTED_URL_SOURCE = 'foo';
+
+    protected function getVatNumberValidationRestService(): VatNumberValidationServiceInterface
+    {
+        return new VatNumberValidationRestExpansibleService(
+            self::EXPECTED_URL_SOURCE,
+            $this->client,
+            $this->faultCodeExceptionFactory
+        );
+    }
+}
