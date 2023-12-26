2a08665d1bd326b4b975a823531cc99fdb7b926c
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-26 15:03:18 +0400

    [ECVS-5] added test case: thrown client exception, response empty.

--- tests/unit/services/VatNumberValidationServiceTest.php
+++ tests/unit/services/VatNumberValidationServiceTest.php
@@ -887,6 +887,30 @@ abstract class VatNumberValidationServiceTest extends TestCase
                     null
                 ),
             ],
+            'thrown client exception, response empty' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'thrownRequestException' => $this->getClientExceptionMock([
+                    'response' => $this->getResponseMock(['body' => '']),
+                ]),
+                'expectedException' => new UnknownServiceErrorException(
+                    '',
+                    '',
+                    0,
+                    null
+                ),
+            ],
             'thrown client exception, error code set, error code INVALID_INPUT, error message set' => [
                 'vatNumber' => new VatNumber(
                     'DE',
