e37ca41ffe1814afa8a177581d96485d107546fa
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-27 14:49:29 +0400

    [ECVS-5] added test case: thrown client exception, error code set, error code IP_BLOCKED.

--- tests/unit/services/VatNumberValidationServiceTest.php
+++ tests/unit/services/VatNumberValidationServiceTest.php
@@ -1325,6 +1325,75 @@ abstract class VatNumberValidationServiceTest extends TestCase
                     null
                 ),
             ],
+            'thrown client exception, error code set, error code IP_BLOCKED, error message set' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'thrownRequestException' => $this->getClientExceptionMock([
+                    'response' => $this->getResponseMock(['body' => '{"errorWrappers": [{"error": "IP_BLOCKED", "message": "bar"}]}']),
+                ]),
+                'expectedException' => new IPBlockedServiceException(
+                    'bar',
+                    0,
+                    null
+                ),
+            ],
+            'thrown client exception, error code set, error code IP_BLOCKED, error message empty' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'thrownRequestException' => $this->getClientExceptionMock([
+                    'response' => $this->getResponseMock(['body' => '{"errorWrappers": [{"error": "IP_BLOCKED", "message": ""}]}']),
+                ]),
+                'expectedException' => new IPBlockedServiceException(
+                    '',
+                    0,
+                    null
+                ),
+            ],
+            'thrown client exception, error code set, error code IP_BLOCKED, error message not set' => [
+                'vatNumber' => new VatNumber(
+                    'DE',
+                    '12312312'
+                ),
+                'checkVatCallArgs' => [
+                    $this::EXPECTED_URL_SOURCE,
+                    [
+                        'json' => [
+                            'countryCode' => 'DE',
+                            'vatNumber' => '12312312',
+                        ],
+                    ]
+                ],
+                'thrownRequestException' => $this->getClientExceptionMock([
+                    'response' => $this->getResponseMock(['body' => '{"errorWrappers": [{"error": "IP_BLOCKED"}]}']),
+                ]),
+                'expectedException' => new IPBlockedServiceException(
+                    '',
+                    0,
+                    null
+                ),
+            ],
             // TODO: more test cases needed
             // TODO
             'thrown server exception, error code unknown, error message set' => [
