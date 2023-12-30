38b51fd00fe11c78bcb68303fb2cef59897b975f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-30 17:45:44 +0400

    [ECVS-9] removed test.

--- tests/unit/helpers/ResponseFactoryTest.php
+++ tests/unit/helpers/ResponseFactoryTest.php
@@ -6,8 +6,6 @@ use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
-use rocketfellows\ViesVatValidationInterface\VatNumber;
-use rocketfellows\ViesVatValidationInterface\VatNumberValidationResult;
 use rocketfellows\ViesVatValidationRest\helpers\ResponseFactory;
 use stdClass;
 
@@ -86,86 +84,6 @@ class ResponseFactoryTest extends TestCase
             'response body bool' => [
                 'response' => $this->getResponseMock(['body' => 'true']),
                 'expectedResponseData' => (object) [],
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider getVatNumberValidationResultProvidedData
-     */
-    public function testGetVatNumberValidationResult(
-        stdClass $responseData,
-        VatNumberValidationResult $expectedVatNumberValidationResult
-    ): void {
-        $this->assertEquals(
-            $expectedVatNumberValidationResult,
-            ResponseFactory::getVatNumberValidationResult($responseData)
-        );
-    }
-
-    public function getVatNumberValidationResultProvidedData(): array
-    {
-        return [
-            'country code set, vat number set, request date set, validation flag set true, name set, address set' => [
-                'responseData' => (object) [
-                    'countryCode' => 'DE',
-                    'vatNumber' => '1234',
-                    'requestDate' => '2023-12-12 10:10:10',
-                    'valid' => true,
-                    'name' => 'foo',
-                    'address' => 'bar',
-                ],
-                'expectedVatNumberValidationResult' => new VatNumberValidationResult(
-                    (new VatNumber('DE', '1234')),
-                    '2023-12-12 10:10:10',
-                    true,
-                    'foo',
-                    'bar'
-                ),
-            ],
-            'country code set, vat number set, request date set, validation flag set false, name set, address set' => [
-                'responseData' => (object) [
-                    'countryCode' => 'DE',
-                    'vatNumber' => '1234',
-                    'requestDate' => '2023-12-12 10:10:10',
-                    'valid' => false,
-                    'name' => 'foo',
-                    'address' => 'bar',
-                ],
-                'expectedVatNumberValidationResult' => new VatNumberValidationResult(
-                    (new VatNumber('DE', '1234')),
-                    '2023-12-12 10:10:10',
-                    false,
-                    'foo',
-                    'bar'
-                ),
-            ],
-            'country code empty, vat number empty, request date empty, validation flag empty, name empty, address empty' => [
-                'responseData' => (object) [
-                    'countryCode' => '',
-                    'vatNumber' => '',
-                    'requestDate' => '',
-                    'valid' => false,
-                    'name' => '',
-                    'address' => '',
-                ],
-                'expectedVatNumberValidationResult' => new VatNumberValidationResult(
-                    (new VatNumber('', '')),
-                    '',
-                    false,
-                    '',
-                    ''
-                ),
-            ],
-            'country code not set, vat number not set, request date not set, validation flag not set, name not set, address not set' => [
-                'responseData' => (object) [],
-                'expectedVatNumberValidationResult' => new VatNumberValidationResult(
-                    (new VatNumber('', '')),
-                    '',
-                    false,
-                    '',
-                    ''
-                ),
             ],
         ];
     }
